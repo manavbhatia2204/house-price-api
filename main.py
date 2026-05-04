877bafc436a982d08ee4e40e8c1cf8340b00c9bc
Manav Bhatia <manavkepal.bhatia2@mail.dcu.ie>
2026-05-04 21:08:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+model = joblib.load("model.pkl")
+
+
+class HouseInput(BaseModel):
+    GrLivArea: float
+    OverallQual: float
+    GarageCars: float
+
+
+@app.get("/")
+def home():
+    return {"message": "House Price Prediction API is running"}
+
+
+@app.post("/predict")
+def predict(data: HouseInput):
+    input_data = pd.DataFrame([data.dict()])
+    
+    input_data = input_data.reindex(
+        columns=model.feature_names_in_,
+        fill_value=0
+    )
+    
+    prediction = model.predict(input_data)[0]
+    
+    return {
+        "predicted_price": float(prediction)
+    }
